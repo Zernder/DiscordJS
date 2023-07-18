a62f278efd39d3c56f1b71f9bf6599365059e286
Zernder <102071021+Zernder@users.noreply.github.com>
2023-07-18 01:36:36 -0700

    update1

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -2,47 +2,47 @@ const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('kick')
-		.setDescription('Kicks a member from the server')
+		.setName('ban')
+		.setDescription('bans a member from the server')
 		.addUserOption((option) =>
 			option
 				.setName('member')
-				.setDescription('The member to kick')
+				.setDescription('The member to ban')
 				.setRequired(true),
 		)
 		.addStringOption((option) =>
-			option.setName('reason').setDescription('The reason for the kick'),
+			option.setName('reason').setDescription('The reason for the ban'),
 		),
 	async execute(interaction) {
 		const member = interaction.options.getMember('member');
 		const reason =
       interaction.options.getString('reason') || 'No reason provided';
 
-		if (!interaction.member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
+		if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
 			return interaction.reply({
 				content: 'You do not have permission to use this command.',
 				ephemeral: true,
 			});
 		}
 
-		if (!member.kickable) {
+		if (!member.Bannable) {
 			return interaction.reply({
-				content: 'The member cannot be kicked.',
+				content: 'The member cannot be banned.',
 				ephemeral: true,
 			});
 		}
 
 		try {
-			await member.kick(reason);
+			await member.ban(reason);
 			interaction.reply({
-				content: `Successfully kicked ${member.user.tag}`,
+				content: `Successfully banned ${member.user.tag}`,
 				ephemeral: true,
 			});
 		}
 		catch (error) {
 			console.error(error);
 			interaction.reply({
-				content: 'An error occurred while trying to kick the member.',
+				content: 'An error occurred while trying to ban the member.',
 				ephemeral: true,
 			});
 		}
